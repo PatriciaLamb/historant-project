923e67d04be6c75d829726c7b64e10ad6f24f6a3
Patricia Lambertz <lambertz.patricia@gmail.com>
2023-03-14 10:56:33 +0100

    debug

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,13 +1,13 @@
 class Place < ApplicationRecord
   include PgSearch::Model
   belongs_to :user
-  
+
   validates :name, presence: true
 
+  has_many :menu_items # dependent: :destroy
+
   accepts_nested_attributes_for :menu_items
-  
-  has_many :menu_items, dependent: :destroy
-  
+
   geocoded_by :address
 
   after_validation :geocode, if: :will_save_change_to_address?
