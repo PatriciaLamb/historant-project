cfe0824fccafdb6b4fff2ddb45ccab5eb97ca45c
Latif Turaan <latiftoran@gmail.com>
2023-03-14 11:58:10 +0100

    pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.hosts << /\A[a-z\d-]+\.loca\.lt\z/
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
