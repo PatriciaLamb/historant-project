377a131d596ec69d44d7b227b58492d7607018a9
Zahra Ilkhan <zahraa1991@gmail.com>
2023-03-14 11:36:34 +0100

    add ten places

--- app/javascript/controllers/choose_place_controller.js
+++ app/javascript/controllers/choose_place_controller.js
@@ -2,9 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="choose-place"
 export default class extends Controller {
-  static targets =  ['select', 'hide']
+  static targets =  ['select', 'newPlace']
+
 
   hide() {
-    console.log("object");
+    // console.log(this.selectTarget);
+    // console.log(this.newPlaceTarget);
+    // console.log(this.newPlaceTarget.style.display);
+    this.newPlaceTarget.style.display = 'none';
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,49 +19,127 @@ user = User.create(
   last_name: "Lambertz"
 )
 
-mcdonalds = Place.new(
+sunshine = Place.new(
   {
-    name: "McDonalds",
-    address: "Rudolfplatz 2, Cologne",
+    name: "Cafe Sunshine",
+    address: "Richard-Wagner-Str. 12, 50674 Cologne, Germany",
+    cuisine: "Spanish",
+    category: "Cafe"
+  }
+)
+
+sunshine.user = user
+sunshine.save!
+
+chez = Place.new(
+  {
+    name: "Chez Céline",
+    address: "127 Rue de Bagnolet, 75020 Paris, France",
+    cuisine: "French",
+    category: "Restaurant"
+  }
+)
+
+chez.user = user
+chez.save!
+
+nolbu = Place.new(
+  {
+    name: "Nolbu",
+    address: "Lambachstraße 23, 44145 Dortmund, Germany",
+    cuisine: "Japanese",
+    category: "Restaurant"
+  }
+)
+
+nolbu.user = user
+nolbu.save!
+
+hanoi = Place.new(
+  {
+    name: "Hanoi",
+    address: "Friesenstraße 123, 50670 Cologne, Germany",
+    cuisine: "Vietnamese",
+    category: "Restaurant"
+  }
+)
+
+hanoi.user = user
+hanoi.save!
+
+hawaii = Place.new(
+  {
+    name: "Hawaiian Dream",
+    address: "Hohe Str. 52, 50667 Cologne, Germany",
+    cuisine: "Hawaiian",
+    category: "Bar"
+  }
+)
+
+hawaii.user = user
+hawaii.save!
+
+
+banana = Place.new(
+  {
+    name: "Sweet Banana",
+    address: "Via Nicola Porpora, 55, 10154 Torino TO, Italy",
+    cuisine: "Italian",
+    category: "Cafe"
+  }
+)
+
+banana.user = user
+banana.save!
+
+ragatta = Place.new(
+  {
+    name: "RaGatta",
+    address: "Via del Corso, 181-188, 00186 Roma RM, Italy",
+    cuisine: "Italian",
+    category: "Restaurant"
+  }
+)
+
+ragatta.user = user
+ragatta.save!
+
+cherry = Place.new(
+  {
+    name: "Red Cherry",
+    address: "Wallrafplatz 5, 50667 Cologne, Germany",
+    cuisine: "Italian",
+    category: "Restaurant"
+  }
+)
+
+cherry.user = user
+cherry.save!
+
+happy = Place.new(
+  {
+    name: "Happy Day",
+    address: "Wallrafplatz 5, 50667 Cologne, Germany",
+    cuisine: "Italian",
+    category: "Restaurant"
+  }
+)
+
+happy.user = user
+happy.save!
+
+imi = Place.new(
+  {
+    name: "Imi & Co. Burger & Steakhouse",
+    address: "Stromstraße 41, 40221 Düsseldorf, Germany",
     cuisine: "American",
     category: "Restaurant"
   }
 )
-mcdonalds.user = user
-mcdonalds.save!
 
-mongol = Place.new(
-  {
-    name: "Mongol",
-    address: "Friesenstraße 34, Cologne",
-    category: "Bar"
-  }
-)
-mongol.user = user
-mongol.save!
+imi.user = user
+imi.save!
 
-punjabi = Place.new(
-  {
-    name: "Punjabi Palace",
-    address: "Venloer Straße 53, Cologne",
-    cuisine: "Indian",
-    category: "Restaurant"
-  }
-)
-punjabi.user = user
-punjabi.save!
-
-cafe = Place.new(
-  {
-    name: "Cafe-Coffee-Day",
-    address: "Ehrenstraße 34, Cologne",
-    category: "Cafe"
-  }
-)
-cafe.user = user
-cafe.save!
-
-file = URI.open("https://images.unsplash.com/photo-1589302168068-964664d93dc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmlyeWFuaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
 indian_biryani = MenuItem.new(
   {
     name: "Biryani",
@@ -70,15 +148,16 @@ indian_biryani = MenuItem.new(
     visit_date: Date.today,
     rating: 5,
     description: "Fantastic, with lots of sauce"
-
   }
 )
+
+file = URI.open("https://images.unsplash.com/photo-1589302168068-964664d93dc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmlyeWFuaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
+
 indian_biryani.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 indian_biryani.place = punjabi
 indian_biryani.save!
 
-file = URI.open("https://images.unsplash.com/photo-1516743619420-154b70a65fea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FwcHVjY2lub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
 
 cappuccino_cafe = MenuItem.new(
   {
@@ -90,6 +169,9 @@ cappuccino_cafe = MenuItem.new(
     description: "never again.. tasted like instant coffee"
   }
 )
+
+file = URI.open("https://images.unsplash.com/photo-1516743619420-154b70a65fea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FwcHVjY2lub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
+
 cappuccino_cafe.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 cappuccino_cafe.place = cafe
